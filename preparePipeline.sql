49ed7b1c2158b5fb5dd4ee8de2ec25bb81c86b57
Robson Thanael Poffo <robson.poffo@gmail.com>
2022-05-11 10:53:04 -0400

    Update preparePipeline.sql

--- preparePipeline.sql
+++ preparePipeline.sql
@@ -1,1 +1,14 @@
+-- SELECT
+--  table_name, ddl
+-- FROM
+--  `fb8960e5938247ef9d0de34157de0f42`.INFORMATION_SCHEMA.TABLES
+-- WHERE
+--  table_name="aaaa_execution_prepare"
+
+-- CREATE TABLE `carol-fb8960e5938247ef9d0d.fb8960e5938247ef9d0de34157de0f42.aaaa_execution_prepare`
+-- (
+--   dateRun DATE,
+--   detail STRING
+-- );
+
 insert into aaaa_execution_prepare values(current_date(), "running prepare PIPELINE script now!!!");
