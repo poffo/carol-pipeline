dd6a8773f4fe5de0140152b516ec68759a88f537
Robson Thanael Poffo <robson.poffo@gmail.com>
2022-05-11 10:49:14 -0400

    Update prepare.sql

--- prepare.sql
+++ prepare.sql
@@ -1,1 +1,1 @@
-insert into `fb8960e5938247ef9d0de34157de0f42`.aaaa_execution_prepare values(current_date(), "running prepare script now!!!");
+insert into aaaa_execution_prepare values(current_date(), "running prepare script now!!!");
