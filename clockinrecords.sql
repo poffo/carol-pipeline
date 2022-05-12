ba2ffc443deee7c8d6948a19bd606a7c2a5c4ebc
Robson Thanael Poffo <robson.poffo@gmail.com>
2022-05-12 01:55:40 -0400

    Update clockinrecords.sql

--- clockinrecords.sql
+++ clockinrecords.sql
@@ -2,10 +2,19 @@
     select [
     struct("5050" as devicecode, "test" as appname, "asdadadsasd" as piscode, current_datetime() as eventdatestr)
     ] as clockinrecords
+    ),
+    c as (
+        select deviceCode, appname, "na" as piscode, clockinDatetime eventdatestr
+        from stg_clockinmobile_clockinrecords
+    ),
+    AC as (
+        select * except(clockinrecords) from a, unnest (clockinrecords)
+        union all
+        select * from c
     )
 
-    select * except(clockinrecords),
+    select *,
         piscode as __mdmId,
         0 as __mdmCounterForEntity
 
-    from a, unnest (clockinrecords)
+    from AC
